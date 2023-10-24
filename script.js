196458de84b9d63abd19f38525506eac41864cd8
majoros.boti <majoros.botesz@gmail.com>
2023-10-24 11:22:45 +0200

    készen van!

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const button = document.querySelector("button");
 button.addEventListener("click", createResult);
 
+const type = {
+    "adult" : 350,
+    "student" : 175,
+    "retired" : 100,
+    "animal" : 50
+}
+
 function createResult(){
     const quantity = document.querySelector(".myContainer div input");
     const ticketType = document.querySelector(".myContainer div:nth-child(2) select");
@@ -17,8 +24,17 @@ function createResult(){
 
     discount = quantity.value >= 10 ? 0.9 : 1;
 
-    result.value = ticketType-value === "adult" ? `${quantity*350* discount}Ft` : `${quantity * 175 * discount}Ft`;
+/*  result.value = ticketType-value === "adult" ? `${quantity*350* discount}Ft` : `${quantity * 175 * discount}Ft`;*/
 
+if(ticketType.value === "animal" && quantity.value > 1){
+    quantity.value = "";
+    quantity.focus();
+    discount = 1;
+    alert("Sok az állat!")
+}
+//amikor több állatot irnak be akkor a quantity.value értéke üres sztring lesz.
+//mégis müködik a szorzás itt lenn, mert "" --> 0 lesz!  Inplicit konverzió!
+    result.value = quantity.value * discount * type[ticketType.value];
     
 
 }
